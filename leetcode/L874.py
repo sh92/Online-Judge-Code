a5f32210ca70c1de9604334749698b5eb03bdbd7
sh92 <sanghee.lee1992@gmail.com>
2018-07-13 14:08:57 +0900

    Change L874

--- leetcode/L874.py
+++ leetcode/L874.py
@@ -1,84 +1,65 @@
-class MyCircularDeque:
+class MyCircularQueue:
 
     def __init__(self, k):
         """
-        Initialize your data structure here. Set the size of the deque to be k.
+        Initialize your data structure here. Set the size of the queue to be k.
         :type k: int
         """
-        self.mylist = []
+        self.mylist = [0 for i in range(k)]
         self.maxSize = k
         self.size = 0
         self.front = 0
-        self.rear = k - 1
+        self.rear = 0
+        
 
-    def insertFront(self, value):
+    def enQueue(self, value):
         """
-        Adds an item at the front of Deque. Return true if the operation is successful.
+        Insert an element into the circular queue. Return true if the operation is successful.
         :type value: int
         :rtype: bool
         """
         if self.isFull():
             return False
-        self.mylist.insert(self.front,value)
+        self.mylist[self.rear] = value
         self.size = self.size + 1
-        return True
-
-    def insertLast(self, value):
-        """
-        Adds an item at the rear of Deque. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
-        if self.isFull():
-            return False
-        self.mylist.insert(self.rear, value)
-        self.size = self.size + 1
+        self.rear = (self.rear + 1) % (self.maxSize)
         return True
         
 
-    def deleteFront(self):
+    def deQueue(self):
         """
-        Deletes an item from the front of Deque. Return true if the operation is successful.
+        Delete an element from the circular queue. Return true if the operation is successful.
         :rtype: bool
         """
         if self.isEmpty():
-            return False
-        self.front = self.front + 1
-        self.size = self.size - 1
+           return False
+        self.front = (self.front + 1) % self.maxSize
+        self.size -= 1
         return True
-        
 
-    def deleteLast(self):
+    def Front(self):
         """
-        Deletes an item from the rear of Deque. Return true if the operation is successful.
-        :rtype: bool
-        """
-        if self.isEmpty():
-            return False
-        self.front = self.front + 1
-        self.size = self.size - 1
-        return True 
-        
-
-    def getFront(self):
-        """
-        Get the front item from the deque.
+        Get the front item from the queue.
         :rtype: int
         """
+        if self.isEmpty():
+            return -1
         return self.mylist[self.front]
         
 
-    def getRear(self):
+    def Rear(self):
         """
-        Get the last item from the deque.
+        Get the last item from the queue.
         :rtype: int
         """
-        return self.mylist[self.rear]
+        if self.isEmpty():
+            return -1
+        return self.mylist[self.rear-1]
         
 
     def isEmpty(self):
         """
-        Checks whether the circular deque is empty or not.
+        Checks whether the circular queue is empty or not.
         :rtype: bool
         """
         return (self.size == 0)
@@ -86,20 +67,59 @@ class MyCircularDeque:
 
     def isFull(self):
         """
-        Checks whether the circular deque is full or not.
+        Checks whether the circular queue is full or not.
         :rtype: bool
         """
         return (self.size == self.maxSize)
         
+def excute_code( obj, methods, params):
+    for idx in range(0,len(methods)):
+        print(locals()[methods[idx]](params[idx][0]))
 
 
-# Your MyCircularDeque object will be instantiated and called as such:
-obj = MyCircularDeque(3)
-param_1 = obj.insertFront(1)
-param_2 = obj.insertLast(2)
-param_3 = obj.deleteFront()
-param_4 = obj.deleteLast()
-param_5 = obj.getFront()
-param_6 = obj.getRear()
-param_7 = obj.isEmpty()
-param_8 = obj.isFull()
+# Your MyCircularQueue object will be instantiated and called as such
+method = ["enQueue", "Rear","Front","deQueue","Front","deQueue","Front","enQueue","enQueue","enQueue","enQueue"]
+param = [[2],[],[],[],[],[],[],[4],[2],[2],[3]]
+
+
+obj = MyCircularQueue(3)
+print("rear",obj.Rear())
+print("enque ",obj.enQueue(1))
+print("rear",obj.Rear())
+print("enque ",obj.enQueue(2))
+print("rear",obj.Rear())
+print("enque ",obj.enQueue(3))
+print("rear",obj.Rear())
+print("enque ",obj.enQueue(4))
+print("rear",obj.Rear())
+print("isFull",obj.isFull())
+print("rear",obj.Rear())
+print("deQueue",obj.deQueue())
+print("rear",obj.Rear())
+print("enQueue",obj.enQueue(4))
+print("rear",obj.Rear())
+
+'''
+k = 3
+obj = MyCircularQueue(k)
+print("enque ",obj.enQueue(1))
+print("enque 2 ",obj.enQueue(2))
+print("enque 3 ",obj.enQueue(3))
+print("enque 4 ",obj.enQueue(4))
+print("deque ",obj.deQueue())
+print("enque 5 ",obj.enQueue(5))
+print("Front : ", obj.Front())
+print("Rear : ",obj.Rear())
+print("enque 6 ",obj.enQueue(6))
+print("Empty : ",obj.isEmpty())
+print("Full : " , obj.isFull())
+print("deque ",obj.deQueue())
+print("Empty : ",obj.isEmpty())
+print("Full : " , obj.isFull())
+print("deque ",obj.deQueue())
+print("Empty : ",obj.isEmpty())
+print("Full : " , obj.isFull())
+print("deque ",obj.deQueue())
+print("Empty : ",obj.isEmpty())
+print("Full : " , obj.isFull())
+'''
